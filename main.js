9fca2dc2c8b1cb1c665b2620f14e585581d02b86
Roxane Dulhoste <ROXANE@MacBook-Air-de-Roxane.local>
2016-12-14 13:45:32 +0100

    start

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(main);
+
+function main() {
+
+$('a[href^="#"]').click(function() {
+        var the_id = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(the_id).offset().top
+        }, 'slow');
+        return false;
+    });
+
+  }
